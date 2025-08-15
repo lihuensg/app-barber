6778681a8be4adf58c129c49b1feed72221722a5
lihuen <lihuensg@gmail.com>
2025-08-15 11:37:26 -0300

    mostrar foto de perfil en el post

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -4,17 +4,20 @@ dotenv.config();
 
 const usarNeon = process.env.USE_NEON === "true";
 
-export const sequelize = new Sequelize(
-  usarNeon ? process.env.DB_REMOTE_NAME : process.env.DB_LOCAL_NAME,
-  usarNeon ? process.env.DB_REMOTE_USER : process.env.DB_LOCAL_USER,
-  usarNeon ? process.env.DB_REMOTE_PASSWORD : process.env.DB_LOCAL_PASSWORD,
-  {
-    host: usarNeon ? process.env.DB_REMOTE_HOST : process.env.DB_LOCAL_HOST,
-    port: usarNeon ? process.env.DB_REMOTE_PORT : process.env.DB_LOCAL_PORT,
-    dialect: "postgres",
-    dialectOptions: usarNeon
-      ? { ssl: { require: true, rejectUnauthorized: false } } // necesario para Neon
-      : {},
-    logging: false
-  }
-);
+export const sequelize = usarNeon
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+      logging: false
+    })
+  : new Sequelize(
+      process.env.DB_LOCAL_NAME,
+      process.env.DB_LOCAL_USER,
+      process.env.DB_LOCAL_PASSWORD,
+      {
+        host: process.env.DB_LOCAL_HOST,
+        port: process.env.DB_LOCAL_PORT,
+        dialect: "postgres",
+        logging: false
+      }
+    );
